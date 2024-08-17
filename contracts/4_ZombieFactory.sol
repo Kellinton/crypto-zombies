5fe41e6f66d8be76b99f2e285efd2940a020ad49
Kellinton <kellintonbrasil@gmail.com>
2024-08-17 19:15:07 -0300

    feat: estrutura Zombie e array para armazenar zumbis gerados

--- contracts/4_ZombieFactory.sol
+++ contracts/4_ZombieFactory.sol
@@ -8,4 +8,13 @@ contract ZombieFactory {
     // Módulo para garantir que o DNA esteja dentro da faixa desejada
     uint dnaModulus = 10 ** dnaDigits;
 
+    // Estrutura que define um Zumbi com nome e DNA
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // Array público para armazenar todos os zumbis criados
+    Zombie[] public zombies;
+
 }
